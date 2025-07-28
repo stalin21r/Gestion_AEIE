ee13205f491955c451ecd30c7c7b96868c4ee707
stalin21r <maick_rony@live.com>
2025-07-28 14:57:21 -0500

    Cambios actuales

--- backend/src/asistencia/asistencia.module.ts
+++ backend/src/asistencia/asistencia.module.ts
@@ -3,8 +3,8 @@ import { AsistenciaService } from './asistencia.service'
 import { AsistenciaController } from './asistencia.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Asistencia } from './entities/asistencia.entity'
-import { AuthModule } from 'src/auth/auth.module'
-import { Usuario } from 'src/usuario/entities/usuario.entity'
+import { AuthModule } from '../auth/auth.module'
+import { Usuario } from '../usuario/entities/usuario.entity'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Asistencia, Usuario]), AuthModule],
